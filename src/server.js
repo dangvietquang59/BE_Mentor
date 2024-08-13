7cc606f5d0ff024f5790736a8799cf3366b2d178
dangvietquang59 <dvquang5902@gmail.com>
2024-08-13 19:57:10 +0700

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -6,12 +6,17 @@ const postRoutes = require("./routes/posts");
 const freetimeRoutes = require("./routes/freetime");
 const bookingRoutes = require("./routes/booking");
 const app = express();
-
+const cors = require("cors");
 db();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
