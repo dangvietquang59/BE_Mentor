6c9d72ccc86dd0ec2daef92351ffca5672a39920
dangvietquang59 <dvquang5902@gmail.com>
2024-10-05 20:58:54 +0700

    fix: api technologies

--- src/controllers/technologiesController.js
+++ src/controllers/technologiesController.js
@@ -1,17 +1,107 @@
 const Technologies = require("../models/Technologies");
 
+// Get all technologies with pagination
 async function getAll(req, res) {
   try {
-    const technologies = await Technologies.find();
-    return res.status(200).json(technologies);
+    const { page = 1, limit = 10 } = req.query;
+
+    // Convert page and limit to numbers
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+
+    const technologies = await Technologies.find()
+      .skip((pageNumber - 1) * limitNumber)
+      .limit(limitNumber);
+
+    const totalItems = await Technologies.countDocuments();
+
+    return res.status(200).json({
+      totalItems,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(totalItems / limitNumber),
+      technologies,
+    });
   } catch (error) {
-    console.log("Error fetching technologies", error);
+    console.error("Error fetching technologies:", error);
     res
       .status(500)
       .json({ error: "An error occurred while fetching the technologies." });
   }
 }
 
+// Create a new technology
+async function create(req, res) {
+  try {
+    const { name } = req.body;
+
+    // Check if the technology already exists
+    const existingTechnology = await Technologies.findOne({ name });
+    if (existingTechnology) {
+      return res.status(400).json({ error: "Technology already exists." });
+    }
+
+    const technology = new Technologies({ name });
+    await technology.save();
+
+    return res.status(201).json(technology);
+  } catch (error) {
+    console.error("Error creating technology:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the technology." });
+  }
+}
+
+// Update an existing technology
+async function update(req, res) {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const technology = await Technologies.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+
+    if (!technology) {
+      return res.status(404).json({ error: "Technology not found." });
+    }
+
+    return res.status(200).json(technology);
+  } catch (error) {
+    console.error("Error updating technology:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the technology." });
+  }
+}
+
+// Delete a technology
+async function remove(req, res) {
+  try {
+    const { id } = req.params;
+
+    const technology = await Technologies.findByIdAndDelete(id);
+
+    if (!technology) {
+      return res.status(404).json({ error: "Technology not found." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Technology deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting technology:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the technology." });
+  }
+}
+
 module.exports = {
   getAll,
+  create,
+  update,
+  remove,
 };
--- src/routes/technologies.js
+++ src/routes/technologies.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const TechnologiesController = require("../controllers/technologiesController");
-
+const authMiddleware = require("../middlewares/authMiddleware");
 router.get("/", TechnologiesController.getAll);
+
+router.post(
+  "/",
+  authMiddleware.authenticateToken,
+  TechnologiesController.create
+);
+
+router.put("/:id", TechnologiesController.update);
+
+router.delete(
+  "/:id",
+  authMiddleware.authenticateToken,
+  TechnologiesController.remove
+);
+
 module.exports = router;
